9e3ea76c7cc4f72ce48b519a1acd0a741b52355f
Stijn Dusseldorp <stijn@ardusit.nl>
2023-06-24 21:54:26 +0200

    Added a card component

--- src/app/pages/products/components/products.component.ts
+++ src/app/pages/products/components/products.component.ts
@@ -2,8 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
+  public products: any[];
 
+  constructor() {
+    this.products = [
+      {
+        id: 1,
+        name: 'Product 1',
+        description: 'Description for product 1',
+        price: 100,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        description: 'Description for product 2',
+        price: 200,
+      },
+    ];
+  }
 }
