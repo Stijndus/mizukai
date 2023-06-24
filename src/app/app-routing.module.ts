6452a7549a109e6a99114fc2d686d08e31892d27
Stijn Dusseldorp <stijn@ardusit.nl>
2023-06-24 20:55:10 +0200

    Created multible useable components and installed bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/components/home.component';
+import { ProductsComponent } from './pages/products/components/products.component';
+import { NotFoundComponent } from './pages/errors/not-found/components/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home',  loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'products',  loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  { path: 'home', component: HomeComponent},
+  { path: 'products',  component: ProductsComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [NavComponent],
-  imports: [CommonModule],
-  exports: [NavComponent],
+  declarations: [NavComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, NgbModule],
+  exports: [NavComponent, HeaderComponent],
 })
 export class LayoutModule {}
