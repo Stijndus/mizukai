4fa91aeeb5490f15a1faa4d8e5ae5f9b8aba77ac
Stijn Dusseldorp <stijn@ardusit.nl>
2023-06-24 17:25:41 +0200

    Renamed it to the products

--- src/app/pages/products/products-routing.module.ts
+++ src/app/pages/products/products-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductComponent } from './components/product.component';
+import { ProductsComponent } from './components/products.component';
 
 const routes: Routes = [
-  { path: '', component: ProductComponent },
+  { path: '', component: ProductsComponent },
 ];
 
 @NgModule({
